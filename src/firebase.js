e8ca632538ef03131ca992cfe6a051b40e1f4145
benduse <benjaminiyodusenga@gmail.com>
2025-08-26 22:54:03 -0500

    replace hardcoded Firebase config with environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCjtKQ7UoV3j4taDn6bNi3ONRlMsBaijBU",
-  authDomain: "wordsearch-6f3e1.firebaseapp.com",
-  projectId: "ejowordsearch",
-  storageBucket: "ejowordsearch.firebasestorage.app",
-  messagingSenderId: "29577044517",
-  appId: "1:29577044517:web:98ab30b2969d5fea0c55cc",
-  measurementId: "G-DQRLWF0FWC"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 export const app = initializeApp(firebaseConfig);
